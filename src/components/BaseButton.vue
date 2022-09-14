a9eef983d7ec4ff0ce7d48c333862c2240bc3639
AfzalSabbir <afzalbd1@gmail.com>
2022-09-14 23:59:36 +0600

    First commit

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,11 +1,52 @@
 <template>
-$END$
+  <div style="display: flex; gap: 8px; justify-content: center;">
+    <button class="base-button" @click="addCount">
+      Add number
+    </button>
+    <button class="base-button" @click="resetCount">
+      Reset
+    </button>
+    <button class="base-button" @click="subCount">
+      Sub number
+    </button>
+  </div>
+
+  <Count ref="countRef"/>
 </template>
 
 <script>
-export default {
-name: "BaseButton"
-}
+import Count                             from "@/components/Count";
+import {defineComponent, onMounted, ref} from "vue";
+
+export default defineComponent({
+  name      : "BaseButton",
+  components: {Count},
+  setup() {
+    const countRef = ref(null)
+    const addCount = () => {
+      countRef.value.addNumber();
+    };
+
+    const resetCount = () => {
+      countRef.value.resetNumber();
+    };
+
+    const subCount = () => {
+      countRef.value.subNumber();
+    };
+
+    onMounted(() => {
+      resetCount();
+    })
+
+    return {
+      addCount,
+      resetCount,
+      subCount,
+      countRef,
+    };
+  },
+})
 </script>
 
 <style scoped>
--- src/components/Count.vue
+++ src/components/Count.vue
@@ -1,11 +1,36 @@
 <template>
-$END$
+  <div class="count">
+    <h1>Count: {{ count }}</h1>
+  </div>
 </template>
 
 <script>
-export default {
-name: "Count"
-}
+import {defineComponent, ref} from "vue";
+
+export default defineComponent({
+  name: "Count",
+  setup(_, context) {
+    const count = ref();
+
+    const addNumber = () => {
+      count.value++;
+    };
+
+    const resetNumber = () => {
+      count.value = 0;
+    };
+
+    const subNumber = () => {
+      count.value--;
+    };
+
+    context.expose({addNumber, resetNumber, subNumber})
+
+    return {
+      count,
+    }
+  },
+})
 </script>
 
 <style scoped>
